47a7666dccc24a8203299e03353a4346d647f985
Sun <30409932+Origintk@users.noreply.github.com>
2020-06-20 08:10:43 -0300

    upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+description 'Fix interiors Conexao Roleplay'
+name 'Conexão Roleplay Fix Mlos'
+author 'Conexão Roleplay'
+
+files {
+    "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+fx_version 'adamant'
+games {'gta5'}
